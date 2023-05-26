05f9b3ca3ca97b69e62b543f145c56c522479487
Fatty <faty.aome.rojas14@gmail.com>
2023-05-25 18:14:52 -0600

    Ya quedo el sitio

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,8 +4,10 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Titulo")
     description = models.TextField(verbose_name="Descripción")
     image = models.ImageField(verbose_name="Imagen", upload_to="projects") #esto es para tener mejor organización dentro de los archivos que se suben para que todo este dentro de un directorio especifico
+    link = models.URLField(verbose_name="Dirección Web", null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     update = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
+   
 
     class Meta:
         verbose_name = "Proyecto"
